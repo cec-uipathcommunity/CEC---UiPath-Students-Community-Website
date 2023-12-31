90c182a14270b5db052a1094daf53e755dea9fa9
PavanDNaik <pawannaik396@gmail.com>
2024-01-01 02:21:11 +0530

    content for landing page and devDependencies update

--- src/App.js
+++ src/App.js
@@ -8,10 +8,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Landing />}></Route>
-          <Route exact path="/janquiz" element={<Quiz />}></Route>
+          <Route exact path="/quizquest" element={<Quiz />}></Route>
         </Routes>
       </BrowserRouter>
-      {/* <h1 className="text-3l font-bold underline">Hello world!</h1> */}
     </div>
   );
 }
