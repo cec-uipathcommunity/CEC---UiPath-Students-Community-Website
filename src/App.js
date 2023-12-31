cf8e1d88992c67ee451d6433f5b04dcf0fe17338
PavanDNaik <pawannaik396@gmail.com>
2024-01-01 01:57:13 +0530

    tailwind init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Quiz from "./screen/Quiz";
+import Landing from "./screen/Landing";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Landing />}></Route>
+          <Route exact path="/janquiz" element={<Quiz />}></Route>
+        </Routes>
+      </BrowserRouter>
+      {/* <h1 className="text-3l font-bold underline">Hello world!</h1> */}
     </div>
   );
 }
