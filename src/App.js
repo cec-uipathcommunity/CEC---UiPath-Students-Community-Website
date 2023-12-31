e22d9a83aba666603d94b73c6f901a8616d833ea
sakshathrai <sakshathrai2004@gmail.com>
2024-01-01 03:30:50 +0530

    Fixed all prob

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Quiz from "./screen/Quiz";
+import Landing from "./screen/Landing";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Landing />}></Route>
+          <Route exact path="/quizquest" element={<Quiz />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
