1494594525ecb6daaeb870e46a3ec95fe410bcad
sakshathrai <sakshathrai2004@gmail.com>
2024-01-01 11:04:57 +0530

    landing-fixed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Quiz from "./screen/Quiz";
 import Landing from "./screen/Landing";
+import Contact from "./screen/Contact";
+import About from "./screen/About";
 function App() {
   return (
     <div className="App">
@@ -9,6 +11,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Landing />}></Route>
           <Route exact path="/quizquest" element={<Quiz />}></Route>
+          <Route exact path="/about" element={<About />}></Route>
+          <Route exact path="/contact" element={<Contact/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
