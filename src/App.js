e0309f3fecae543e5a7f0f976d011b2fbaba4246
sakshathrai <sakshathrai2004@gmail.com>
2024-01-01 13:03:51 +0530

    landing-fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Quiz from "./screen/Quiz";
 import Landing from "./screen/Landing";
 import Contact from "./screen/Contact";
@@ -7,14 +7,14 @@ import About from "./screen/About";
 function App() {
   return (
     <div className="App">
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Landing />}></Route>
           <Route exact path="/quizquest" element={<Quiz />}></Route>
           <Route exact path="/about" element={<About />}></Route>
           <Route exact path="/contact" element={<Contact/>}></Route>
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
