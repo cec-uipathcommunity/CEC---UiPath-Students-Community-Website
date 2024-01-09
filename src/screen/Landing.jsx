2ad4279a6309528ce86441f25eb1efa5eab2f81a
sakshathrai <sakshathrai2004@gmail.com>
2024-01-09 16:25:46 +0530

    landing and env1

--- src/screen/Landing.jsx
+++ src/screen/Landing.jsx
@@ -1,6 +1,5 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
-import Leaderboard from "../components/Leaderboard";
 export default function Landing() {  
 
   return (
